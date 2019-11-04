2a927f6b1b4f1278eb0ed541486e21c39ef77365
RankerPG <maxfightstar@gmail.com>
2019-11-04 19:28:39 +0900

    솔루션 생성

---
+++ EffectC/EffectC/EffectC.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	A() = default;
+	~A() = default;
+	explicit A(int p_num)
+		: m_iNum(p_num) {}
+
+public:
+	inline const int& Get_Num() { return m_iNum; }
+
+private:
+	int m_iNum;
+};
+
+void ShowClassA(A p_a)
+{
+	cout << p_a.Get_Num() << endl;
+}
+
+int main()
+{
+	// 암시적 형변환의 예
+	//ShowClassA(5);
+}
