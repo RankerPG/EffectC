673c79302f32c3b76d92211f7104b9bc85399d90
rankerpg <maxfightstar@naver.com>
2019-11-14 20:21:44 +0900

    11.14 코드 커밋

---
+++ EffectC/EffectC/191114.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//template <typename C>
+//void Print_2st(const C& p_c)
+//{
+//	typename C::const_iterator iter = p_c.begin();
+//
+//	++iter;
+//
+//	cout << *iter << endl;
+//}
+//
+//int main()
+//{
+//	vector<int> v({ 1, 2 });
+//
+//	Print_2st<vector<int>>(v);
+//
+//	return 0;
+//}
